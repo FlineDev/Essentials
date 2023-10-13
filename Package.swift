92918fc3e305f29e0a1ae923448a5649d8c7b6a8
Cihat Gündüz <commits@cihatguenduez.de>
2023-10-13 21:26:53 +0200

    Remove Vapor-specific foundation libs + rename Shared to Data

--- Package.swift
+++ Package.swift
@@ -6,18 +6,12 @@ let package = Package(
    platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9)],
    products: [
       .library(name: "AppFoundation", targets: ["AppFoundation"]),
-      .library(name: "SharedFoundation", targets: ["SharedFoundation"]),
-      .library(name: "VaporAppFoundation", targets: ["VaporAppFoundation"]),
+      .library(name: "DataFoundation", targets: ["DataFoundation"]),
       .library(name: "TestFoundation", targets: ["TestFoundation"]),
-      .library(name: "VaporTestFoundation", targets: ["VaporTestFoundation"]),
    ],
    dependencies: [
       // Apple
       .package(url: "https://github.com/apple/swift-collections.git", from: "1.0.4"),
-
-      // Vapor
-      .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
-      .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
 
       // FlineDev
       .package(url: "https://github.com/FlineDev/HandySwift.git", branch: "main"),
@@ -47,27 +41,10 @@ let package = Package(
          ]
       ),
       .target(
-         name: "SharedFoundation",  // for code shared between Apple Platform apps and server-side Vapor apps
+         name: "DataFoundation",  // for code shared between Apple Platform apps and server-side Vapor apps
          dependencies: [
             // Apple
             .product(name: "OrderedCollections", package: "swift-collections"),
-
-            // FlineDev
-            .product(name: "HandySwift", package: "HandySwift"),
-
-            // 3rd-Party
-            .product(name: "Tagged", package: "swift-tagged"),
-         ]
-      ),
-      .target(
-         name: "VaporAppFoundation",  // for server-side Vapor applications
-         dependencies: [
-            // Apple
-            .product(name: "OrderedCollections", package: "swift-collections"),
-
-            // Vapor
-            .product(name: "Fluent", package: "fluent"),
-            .product(name: "Vapor", package: "vapor"),
 
             // FlineDev
             .product(name: "HandySwift", package: "HandySwift"),
@@ -82,19 +59,6 @@ let package = Package(
             // FlineDev
             .product(name: "HandySwift", package: "HandySwift"),
             
-            // 3rd-Party
-            .product(name: "CustomDump", package: "swift-custom-dump"),
-         ]
-      ),
-      .target(
-         name: "VaporTestFoundation",  // for test targets in server-side Vapor projects
-         dependencies: [
-            // Vapor
-            .product(name: "XCTVapor", package: "vapor"),
-
-            // FlineDev
-            .product(name: "HandySwift", package: "HandySwift"),
-
             // 3rd-Party
             .product(name: "CustomDump", package: "swift-custom-dump"),
          ]
